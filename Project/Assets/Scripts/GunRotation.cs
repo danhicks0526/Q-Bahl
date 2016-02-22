9d56467bd0563040f40d4b6f06e91d7636a63584
Dan <dmh-26@charter.net>
2016-02-22 15:27:52 -0800

    Added Gun and broken looking code

---
+++ Project/Assets/Scripts/GunRotation.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GunRotation : MonoBehaviour {
+
+	//values that will be set in the Inspector
+	public Vector3 mousePos;
+	public Vector3 targetRotation;
+
+	void Start(){
+	
+	}
+
+
+	// Update is called once per frame
+	void Update()
+	{
+		mousePos = Input.mousePosition;
+		mousePos = new Vector3 (mousePos.x, mousePos.y, mousePos.z);
+		transform.LookAt (mousePos);
+	}  
+}
---
+++ Project/Assets/Scripts/MouseFollow.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseFollow : MonoBehaviour {
+	/*
+	public GameObject player;
+	public Vector3 zAxis;
+	public Vector3 pos;
+
+	*/
+	public Vector3 mousePos;
+	// Use this for initialization
+	void Start () {
+		//player = GameObject.Find ("BallAlien");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		/*
+
+		zAxis = player.transform.forward.normalized;
+		pos = Camera.main.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.y, 0));
+		float temp = pos.z - transform.position.z;
+		transform.rotation =  Quaternion.Euler(new Vector3(pos.x,pos.y,zAxis.z*temp));
+		*/
+
+		//broken ass shiat 
+		//fuck
+		mousePos = Input.mousePosition;
+		Vector3 target = new Vector3 (mousePos.x,mousePos.y, 0);
+		//new try
+		transform.RotateAround (target, Vector3.up, 10 * Time.deltaTime);	}
+}
